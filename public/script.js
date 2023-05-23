8c4096aefea3ae723199d0d1bb5e05e0524774ba
CHNTLL <chantelleguevara@yahoo.com.au>
2023-05-23 14:38:53 +1000

    completed interface of main dashboard by including logo and images, also added another popup that appears when the form is successfully submitted

--- public/script.js
+++ public/script.js
@@ -1,6 +1,8 @@
 const openPopupBtn = document.getElementById('open-popup');
 const closePopupBtn = document.getElementById('close-popup');
 const popupContainer = document.getElementById('popup-container');
+const submitContainer = document.getElementById('submit-container');
+const closeSubmitBtn = document.getElementById('close-submit');
 
 openPopupBtn.addEventListener('click', () => {
   popupContainer.style.display = 'flex';
@@ -8,5 +10,12 @@ openPopupBtn.addEventListener('click', () => {
 
 closePopupBtn.addEventListener('click', () => {
   popupContainer.style.display = 'none';
+  submitContainer.style.display = 'flex';
 });
+
+closeSubmitBtn.addEventListener('click', () => {
+  submitContainer.style.display = 'none';
+});
+
+
 
