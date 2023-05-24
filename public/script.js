3522817cd0b905c44f93de34c79f0f8010574fdf
CHNTLL <chantelleguevara@yahoo.com.au>
2023-05-24 22:26:57 +1000

    begun working on js for form in order to save user input

--- public/script.js
+++ public/script.js
@@ -19,3 +19,40 @@ closeSubmitBtn.addEventListener('click', () => {
 
 
 
+//let session = {
+//  purpose: "Endurance",
+//  stroke: "Freestyle",
+//  distance: 200,
+//  lengthHr: 1,
+//  lengthMin: 30,
+//  averageMin: 1,
+//  averageSec: 45,
+//  id: Date.now(),
+//  date: new Date().toISOString(),
+//}
+
+//console.log(session);
+
+var sessionList = [];
+
+function addSession(purpose, stroke, distance, lengthHr, lengthMin, averageMin, averageSec) {
+  let session = {
+    purpose,
+    stroke,
+    distance,
+    lengthHr,
+    lengthMin,
+    averageMin,
+    averageSec,
+    id: Date.now(),
+    date: new Date().toISOString(),
+  }
+
+  sessionList.push(session);
+}
+
+addSession("Endurance", "Freestyle", "200m", 1, 30, 1, 45);
+
+console.log(sessionList);
+
+//above works tested on replit
